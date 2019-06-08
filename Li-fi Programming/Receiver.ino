001bb8e44b14c35909eb65310f29903529c37868
Adharsh Murali <33362260+adharshmurali@users.noreply.github.com>
2019-06-08 16:28:53 -0400

    Initial Commit

---
+++ Li-fi Programming/Receiver.ino
@@ -0,0 +1,93 @@
+#include <LiquidCrystal.h>
+String inputString = "";  
+boolean stringComplete = false;
+
+int sensorValue = 0;    
+const int LED = 13; 
+
+LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
+      
+void setup() 
+{
+  pinMode(LED,OUTPUT);
+  digitalWrite(LED,LOW);
+  lcd.begin(16, 2);
+  lcd.print("VtoV communication");
+  Serial.begin(1200);
+  inputString.reserve(200);
+  for (int thisPin = 2; thisPin < 6; thisPin++) 
+  {
+    pinMode(thisPin, OUTPUT);
+  }
+}
+
+int Flag=0,sequence=0,robot_start=0;
+
+void loop() 
+{ 
+      if(sequence == 1)
+      {
+          sequence = 0;  
+          digitalWrite(LED,HIGH); 
+          lcd.setCursor(0, 1);
+          lcd.print("Gas LeakageDetected");  
+          delay(1000);
+          digitalWrite(LED,LOW);        
+      }
+    
+      if(sequence == 2)
+      {
+          sequence = 0;  
+          digitalWrite(LED,HIGH);         
+          lcd.setCursor(0, 1);
+          lcd.print("DrowsinessDetected");          
+          delay(1000);
+          digitalWrite(LED,LOW);        
+      }
+
+     if(sequence == 3)
+      {
+          sequence = 0;  
+          digitalWrite(LED,HIGH);         
+          lcd.setCursor(0, 1);
+          lcd.print("Obstacle Detected");          
+          delay(1000);
+          digitalWrite(LED,LOW);        
+      }
+
+      if(sequence == 4)
+      {
+          sequence = 0;  
+          digitalWrite(LED,HIGH);         
+          lcd.setCursor(0, 1);
+          lcd.print("                ");          
+          delay(1000);
+          digitalWrite(LED,LOW);                  
+      }     
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    inputString += inChar;
+
+    if (inChar == 'A') {
+      sequence = 1;
+    }
+    if (inChar == 'B') {
+      //stringComplete = true;
+      sequence = 2;
+    }
+    if (inChar == 'C') {
+      //stringComplete = true;
+      sequence = 3;
+    }    
+    if (inChar == 'D') {
+      //stringComplete = true;
+      sequence = 4;
+    }  
+  }
+}
+
+
+
---
+++ Li-fi Programming/Transmitter.ino
@@ -0,0 +1,94 @@
+const int trigPin    = 2;
+const int echoPin    = 3;
+const int buttonPin  = 4;
+const int analogInPin = A0; 
+
+int sensorValue = 0;   
+int buttonPushCounter = 0;
+int buttonState = 0;         
+int lastButtonState = 0;
+
+    
+void setup() 
+{
+  Serial.begin(1200);
+  pinMode(buttonPin,INPUT);
+}
+
+void loop() 
+{ 
+
+  buttonState = digitalRead(buttonPin);
+  if (buttonState != lastButtonState)
+    {
+ 
+    if (buttonState == HIGH)
+    {
+     
+      buttonPushCounter++;
+//      Serial.println("on");
+//      Serial.print("number of button pushes:  ");
+//      Serial.println(buttonPushCounter);
+    } 
+//    else 
+//    {   
+//      Serial.println("off");
+//    } 
+    delay(50);
+  }
+  
+  lastButtonState = buttonState;
+  
+      sensorValue = analogRead(analogInPin);
+      long duration, inches, cm;
+      pinMode(trigPin, OUTPUT);
+      digitalWrite(trigPin, LOW);
+      delayMicroseconds(2);
+      digitalWrite(trigPin, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(trigPin, LOW);
+      pinMode(echoPin, INPUT);
+      duration = pulseIn(echoPin, HIGH);
+      inches = microsecondsToInches(duration);
+      cm = microsecondsToCentimeters(duration);
+      delay(100);      
+
+      if(sensorValue > 400)
+      {
+        Serial.println('A'); 
+      }
+
+      else if(cm < 10)
+      {
+        Serial.println('C');
+      }
+
+      else if (buttonPushCounter > 10)
+      {    
+         Serial.println('B'); 
+      }
+    
+      else if(buttonPushCounter > 25)
+      {
+        buttonPushCounter = 0;
+      }      
+
+//
+//      else
+//      {
+//        Serial.println('D');
+//      }
+                  
+}
+
+long microsecondsToInches(long microseconds)
+{
+   return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds)
+{
+  return microseconds / 29 / 2;
+}
+
+
